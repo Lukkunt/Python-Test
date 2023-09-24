626984a99706769a4bdd87c038e0c964935d08d6
Lukkunt <lukas.kunt@hardwario.com>
2023-09-24 18:24:00 +0200

    First commit

---
+++ print (45).py
@@ -0,0 +1,16 @@
+def get_sum(a, b):
+    sum_number = 0
+    if a == b:
+        return a
+    else:
+        while (a < b):
+            sum_number += a
+            a += 1
+        while (a > b):
+            sum_number += a
+            a -= 1
+    sum_number += b
+    return sum_number
+
+
+print(get_sum(1, -5))
